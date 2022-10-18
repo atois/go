aad4b59217ddc71333a5887d253240736347f312
atois <atois@hotmail.com>
2022-10-18 20:00:47 +0800

    no chinese

---
+++ array/arraytest3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/text/number"
+)
+
+func main() {
+	num := 10
+	fmt.Printf(format:"num: %T\n", num)
+
+	arr1 := [4]int{1,2,3,4}
+	arr1 := [3]float64{2.15,3.18,6.19}
+	arr1 := [4]int{5,6,7,8}
+	arr1 := [2]string{"hello","world"}
+
+}
