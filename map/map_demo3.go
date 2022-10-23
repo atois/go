2e3ac6415ed7d975ee44c8ff1e7c2927a48ae020
atois <atois@hotmail.com>
2022-10-23 11:03:28 +0800

    1103

--- map/map_demo3.go
+++ map/map_demo3.go
@@ -21,4 +21,22 @@ func main() {
 
 	fmt.Println(map2)
 	//s1 := make(s1[]string)
+
+	map3 := map[string]string{"name": "zhangshan", "age": "28", "sex": "male", "adress": "far away"}
+	fmt.Println(map3)
+
+	s1 := make([]map[string]string, 0, 3)
+	s1 = append(s1, map1)
+	s1 = append(s1, map2)
+	s1 = append(s1, map3)
+
+	for i, val := range s1 {
+		fmt.Printf("%d information : \n", i+1)
+		fmt.Printf("\t name:%s\n", val["name"])
+		fmt.Printf("\t age:%s\n", val["age"])
+		fmt.Printf("\t sex:%s\n", val["sex"])
+		fmt.Printf("\t address:%s\n", val["address"])
+
+	}
+
 }
